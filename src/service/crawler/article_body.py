041411e6721151195b2ad27f1858f91d2537f5ec
joelin109 <joe.lin1009@gmail.com>
2017-05-06 23:06:31 +0800

    parse html

--- src/service/crawler/article_body.py
+++ src/service/crawler/article_body.py
@@ -1,7 +1,5 @@
-from bs4 import BeautifulSoup
-import re
-import requests
-from src.service.crawler.article_body_parse_rule import crawl_http_url_parse_rule, crawl_http_article_body_html
+from src.service.crawler.http_html_crawler import HttpHtmlCrawler
+from src.service.crawler.http_html_parse import HttpURlHtmlParse
 
 
 def request_crawl_article_bodys():
@@ -15,50 +13,13 @@ def request_crawl_article_bodys():
 def crawl_article_body(crawl_url=None):
     _url = '' if crawl_url is None else crawl_url
     # _url = 'https://www.washingtonpost.com/posteverything/wp/2017/02/16/my-grandfather-helped-create-captain-america-for-times-like-these/?utm_term=.c60914db592f'
-    _parse_rule, _del_div_tags = crawl_http_url_parse_rule(_url)
-    if _parse_rule == '':
+
+    _parse = HttpURlHtmlParse(_url)
+    _parse.display()
+    if _parse.html_parse_body_tag == '':
         return 'Please set parse rule first'
     else:
-        print(_url + '  |  ' + _parse_rule)
-        _crawl_body_text, _status = _crawler_article_body(_url, _parse_rule, _del_div_tags)
+        _crawler = HttpHtmlCrawler(_parse)
+        _crawler.start()
+        _crawl_body_text, _count, _status = _crawler.get_crawl_result()
         return _crawl_body_text
-
-
-def _crawler_article_body(url, parse_rule, del_div_tags):
-    _body_parse_rule = parse_rule
-
-    _user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
-    _response = requests.get(url, headers={'User-Agent': _user_agent})
-    _response.encoding = 'utf-8'
-    _soup = BeautifulSoup(_response.text, "lxml")
-
-    # _soup_bodys = _soup.find_all("div", _body_parse_rule)
-    # _soup_bodys = _soup.find_all(lambda tag: tag.get('class') == [_body_parse_rule])
-    # _soup_bodys = _soup.find_all(attrs={'class': re.compile(r'^' + _body_parse_rule + '$')})
-    _soup_bodys = _soup.find_all(attrs={'class': re.compile(r'' + _body_parse_rule)})
-
-    _soup_body_count = len(_soup_bodys)
-    if _soup_body_count == 0:
-        return '', _soup_body_count
-    elif _soup_body_count == 1:
-        _soup_body_html_text = _crawler_article_body_html_text(_soup_bodys[0], del_div_tags)
-        return _soup_body_html_text, _soup_body_count
-    else:
-        _soup_body_html_texts = ''
-        for _soup_body in _soup_bodys:
-            _soup_body_html_text = _crawler_article_body_html_text(_soup_body, del_div_tags)
-            _soup_body_html_texts += _soup_body_html_text + '<p></p><p></p>'
-
-        return _soup_body_html_texts, _soup_body_count
-
-
-def _crawler_article_body_html_text(soup, del_div_tags):
-    _soup_body = crawl_http_article_body_html(soup, del_div_tags)
-    _soup_body_text = str(_soup_body).replace('<a href=', '<a target=\"_blank\" href=')
-    return _soup_body_text
-
-
-def _update_article_body(article_id, body_text):
-    print(article_id)
-
-    return ''
---
+++ src/service/crawler/http_html_crawler.py
@@ -0,0 +1,69 @@
+from bs4 import BeautifulSoup, Comment
+import re
+import requests
+from src.service.crawler.http_html_parse import HttpURlHtmlParse
+from src.service.crawler.http_html_parse_util import clean_reset_body_html
+
+
+class HttpHtmlCrawler:
+    url = ''
+    url_status = 200
+    html_parse_body_tag = ''
+    will_del_h5_tags = ['figcaption', 'script', 'aside', 'video']
+    will_del_div_tags = ['share-icons', 'inner-wrapper', 'article-tags', 'view-content', 'newsletter-signup']
+    soup_body = None
+    soup_match_count = 0
+    soup_html_text = ''
+
+    def __init__(self, parser: HttpURlHtmlParse):
+        self.url = parser.url
+        self.html_parse_body_tag = parser.html_parse_body_tag
+        self.will_del_h5_tags = parser.will_del_h5_tags
+        self.will_del_div_tags = parser.will_del_div_tags
+
+    def start(self):
+        self._start_crawl()
+        self._start_parse()
+
+    def _start_crawl(self):
+        _user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
+        _response = requests.get(self.url, headers={'User-Agent': _user_agent})
+        _response.encoding = 'utf-8'
+        _soup = BeautifulSoup(_response.text, "lxml")
+
+        # _soup_bodys = _soup.find_all("div", _body_parse_rule)
+        # _soup_bodys = _soup.find_all(lambda tag: tag.get('class') == [_body_parse_rule])
+        # _soup_bodys = _soup.find_all(attrs={'class': re.compile(r'^' + _body_parse_rule + '$')})
+        self.soup_body = _soup.find_all(attrs={'class': re.compile(r'' + self.html_parse_body_tag)})
+
+    def _start_parse(self):
+        _soup_body_count = len(self.soup_body)
+        if _soup_body_count >= 1:
+            for _soup_body in self.soup_body:
+                _soup_body_html_text = self._parse_soup_body_html_text(_soup_body)
+                self.soup_html_text += _soup_body_html_text + '<p></p>'
+
+    def _parse_soup_body_html_text(self, soup):
+        self._clean_common_useless_html(soup)
+
+        _soup_body = clean_reset_body_html(soup, self.will_del_div_tags)
+        _soup_body_text = str(_soup_body).replace('<a href=', '<a target=\"_blank\" href=')
+        return _soup_body_text
+
+    def _clean_common_useless_html(self, soup):
+        for element in soup(text=lambda text: isinstance(text, Comment)):
+            element.extract()
+
+        for _del_h5_tag in self.will_del_h5_tags:
+            [s.extract() for s in soup(_del_h5_tag)]
+
+        _del_div_re_classes = ['hidden', '-wrapper']
+        for _del_div_re_class in _del_div_re_classes:
+            [s.extract() for s in soup.find_all(attrs={'class': re.compile(r'' + _del_div_re_class)})]
+
+    def display(self):
+        print(self.url + '  |  ' + self.html_parse_body_tag)
+        print(self.will_del_div_tags)
+
+    def get_crawl_result(self):
+        return self.soup_html_text, self.soup_match_count, self.url_status,
