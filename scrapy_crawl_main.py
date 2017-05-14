39f294a4ed555f3689d17db94aa83f156704c498
joelin109 <joe.lin1009@gmail.com>
2017-05-15 03:50:40 +0800

    crawler

--- scrapy_crawl_main.py
+++ scrapy_crawl_main.py
@@ -1,2 +1,5 @@
 from scrapy import cmdline
-cmdline.execute("scrapy crawl db_book2".split())
+from src.crawler.scrapy.spiders.db_book import crawl_name
+
+_execute = 'scrapy crawl ' + crawl_name
+cmdline.execute(_execute.split())
