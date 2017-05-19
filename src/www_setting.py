e86d2efd423dc9ba08c75a52eb89876072dfcdb4
joelin109 <joe.lin1009@gmail.com>
2017-05-19 22:08:28 +0800

    test aws ec2

--- src/www_setting.py
+++ src/www_setting.py
@@ -1,5 +1,5 @@
 class WwwConfig(object):
     DEBUG = True
     Host_URL = '127.0.0.1'
-    Host_Port = 80
+    Host_Port = 1058
     SECRET_KEY = '736670cb10a600b695a55839ca3a5aa54a7d7356cdef815d2ad6e19a2031182b'
