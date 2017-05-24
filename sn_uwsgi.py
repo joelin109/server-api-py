cf809a6255f6e8418e7ef06c7753bbc0c6e05777
joelin109 <joe.lin1009@gmail.com>
2017-05-25 01:40:49 +0800

    test aws ec2

--- sn_uwsgi.py
+++ sn_uwsgi.py
@@ -4,5 +4,6 @@ _exec = 'uwsgi uwsgi.ini'
 os.system(_exec)
 
 # uwsgi uwsgi.ini
-# ps -ef | grep uwsgi
+
+# ps aux | grep uwsgi      :  ps -ef | grep uwsgi
 # pkill -f uwsgi -9        :         killall -9 uwsgi
