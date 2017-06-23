92159a9d1bf9e15f20060cf1494f33d944eddaf0
joelin109 <joe.lin1009@gmail.com>
2017-06-24 00:19:49 +0800

    auto-deploy

--- fabfile.py
+++ fabfile.py
@@ -3,31 +3,35 @@ from fabric.contrib.files import *
 import os
 
 '''
-$ fab install
-
 $ fab kill
 $ fab start
+$ fab start:dev
+$ fab gunic
 '''
 
 
-def start():
+def start(mode='production'):
     kill()
+
     with settings(warn_only=True):
         result = local('nginx')
 
         if result.succeeded:
-            uwsgi()
+            uwsgi(mode)
             print('start: nginx + uwsgi')
 
 
 # ps aux | grep uwsgi      :  ps -ef | grep uwsgi
 # pkill -f uwsgi -9        :         killall -9 uwsgi
 # port is not available after run uwsgi, should visit from nginx
-def uwsgi():
+def uwsgi(mode='production'):
     with warn_only():
         local('pkill -f uwsgi -9')
 
-    local('uwsgi conf/uwsgi.ini')
+    if mode == 'production':
+        local('uwsgi conf/uwsgi.ini')
+    else:
+        local('uwsgi conf/uwsgi_dev.ini')
 
 
 # ps aux |grep gunicorn      :     ps -ef | grep gunicorn    : pkill -f gunicorn -9
@@ -51,24 +55,59 @@ def gunic():
 def kill():
     # '/usr/local/etc/nginx/nginx.conf'
     with settings(warn_only=True):
-        result = local('pkill -f nginx -9')
+        result_nginx = local('pkill -f nginx -9')
 
     with settings(warn_only=True):
-        result = local('pkill -f uwsgi -9')
-        if result.failed:
-            print('non uwsgi process')
+        result_uwsgi = local('pkill -f uwsgi -9')
 
     with settings(warn_only=True):
-        result = local('pkill -f gunicorn -9')
-        if result.succeeded:
-            print('killed.')
-        else:
-            print('non gunicorn process')
+        result_guni = local('pkill -f gunicorn -9')
 
-            # if exists('env/'):
+    if result_nginx.failed:
+        print('non-uwsgi process')
+    else:
+        print('nginx process killed done.')
+
+    if result_uwsgi.failed:
+        print('non-uwsgi process')
+    else:
+        print('uwsgi process killed done.')
+
+    if result_guni.succeeded:
+        print('gunicorn process killed done.')
+    else:
+        print('non gunicorn process')
 
 
-def install():
+'''
+
+# install ops package for python3.6
+# install necessary package for the project
+
+$ fab install:ops
+$ fab install
+
+'''
+
+
+# install python package
+def install(pip='default'):
+    if pip == 'ops':
+        _pip_install_ops()
+
+    else:
+        _pip_install()
+
+
+def _pip_install_ops():
+    result = local('python3.6 -m pip install -r requirements_ops.txt')
+    if result.succeeded:
+        print('python3.6 -m pip install succeeded')
+    else:
+        print('python3.6 -m pip install failed')
+
+
+def _pip_install():
     _virtualenv = True
     if os.path.isdir('env/') is not True:
         with settings(warn_only=True):
@@ -89,3 +128,27 @@ def install():
             if result.succeeded:
                 local('pip install psycopg2')
                 local('pip install scrapy')
+
+
+def ec2():
+    _ssh_ec2_local_path = '/Volumes/Mac-TBD/Server/aws-ec2'
+    _ssh_ec2_pem_file = 'sing-ub-py.pem'
+    _ssh_ec2 = 'ec2-52-221-45-133.ap-southeast-1.compute.amazonaws.com'
+
+    with prefix('cd ' + _ssh_ec2_local_path):
+        _pem_result = local('chmod 400 ' + _ssh_ec2_pem_file)
+        _ssh_connect_exec = 'ssh -i "%s" ubuntu@%s' % (_ssh_ec2_pem_file, _ssh_ec2)
+
+        if _pem_result.failed:
+            print("pem file failed.")
+        if _pem_result.succeeded:
+            _ssh_connect_result = local(_ssh_connect_exec)
+
+            if _ssh_connect_result.failed:
+                print("ssh connect ec2 failed.")
+            else:
+                print('dfdsfdsf')
+                with prefix('cd /home/ubuntu/api36/server-api-py'):
+                    print('dfdsfdsf')
+                    _ec2_result = run('python -V ')
+                    print(_ec2_result)
