e055c6887fbbb1e06f7d8e1506af3bc77c95fc01
joelin109 <joe.lin1009@gmail.com>
2017-06-28 01:59:03 +0800

    adjust API base class

--- src/service/api/article.py
+++ src/service/api/article.py
@@ -1,64 +1,71 @@
-from flask_restful import Resource, reqparse
-from src.service.api.util import api_response_format, api_response_detail_format, api_request_parse
+from src.service.api.util import Api
 from src.service.model.model_content import ContentArticle
 from src.service.logic.article_logic import ArticleLogic, ArticleListFilter
 
 
-class ArticleApi(Resource):
+# Create or modify article
+class ArticleApi(Api):
     def get(self, post_id=None):
         _name = "Joe" if post_id is None else post_id
         return {"Hello": _name}
 
     def post(self, post_id=None):
-        print('ArticleApiArticleApiArticleApiArticleApiArticleApiArticleApiArticleApiArticleApi')
-        _request_data = api_request_parse()
+        _request_data = self.request_data()
+        if self.permitted is False:
+            return self.without_permit()
+
         print(_request_data)
         _article = _get_request_data(_request_data)
         _logic = ArticleLogic()
         _result = _logic.update_article_body(_article)
 
-        return api_response_detail_format(_result)
+        return self.response_detail(_result)
 
 
-class ArticleStatusApi(Resource):
+class ArticleStatusApi(Api):
     def post(self):
-        _request_data = api_request_parse()
+        _request_data = self.request_data()
+        if self.permitted is False:
+            return self.without_permit()
 
         _article = ContentArticle('')
         _article.id = _request_data["id"]
         _is_recommend = _request_data["is_recommend"]
         _article.is_recommend = True if _is_recommend == 1 or _is_recommend else False
-
         _article.publish_status = _request_data["publish_status"]
 
         _logic = ArticleLogic()
         _request_data['result'] = _logic.update_article_status(_article)
 
-        return api_response_detail_format(_request_data)
+        return self.response_detail(_request_data)
 
 
-class ArticleListApi(Resource):
+class ArticleListApi(Api):
     def get(self):
         pass
 
     def post(self):
-        _request_data = api_request_parse()
+        _request_data = self.request_data()
+        if self.permitted is False:
+            return self.without_permit()
 
         _list_filter = _get_request_data_filter(_request_data)
         _logic = ArticleLogic()
         _result_list, _page = _logic.get_list(_list_filter)
-        return api_response_format(_result_list, _page)
+
+        return self.response(_result_list, _page)
 
 
-class ArticleDetailApi(Resource):
+class ArticleDetailApi(Api):
     def post(self, post_id=None):
-        _request_data = api_request_parse()
+        _request_data = self.request_data()
+
         _article_id = _request_data["id"]
         print(_request_data)
         _logic = ArticleLogic()
         _result_detail = _logic.get_detail(_article_id)
 
-        return api_response_detail_format(_result_detail)
+        return self.response_detail(_result_detail)
 
 
 def _get_request_data(request_data):
--- src/service/api/util.py
+++ src/service/api/util.py
@@ -1,12 +1,48 @@
-from src.service.config import SQLConfig
-from flask_restful import reqparse
+from flask_restful import Resource, reqparse
+from datetime import datetime
 from enum import Enum
+from src.service.config import SQLConfig
 
 
 class Http(Enum):
     Request = 0
     Response = 1
     Response_detail = 2
+
+
+class Api(Resource):
+    token: str = ''
+    permitted: bool = True
+
+    def __init__(self):
+        self.http_start_time = datetime.now()
+
+    def request_data(self, parser=None):
+        self.token = ''
+        self.permitted = True
+        return api_request_parse(parser)
+
+    def response(self, result_list, page):
+        _performance = self._calculate_performance()
+        return api_response_format(result_list, page, _performance)
+
+    def response_detail(self, result_detail):
+        return api_response_detail_format(result_detail)
+
+    def without_permit(self):
+        self.token = ''
+        return api_response_format_err('error token')
+
+    def _calculate_performance(self):
+        _request_end_time = datetime.now()
+        _request_gap = _request_end_time - self.http_start_time
+
+        _performance = {
+            "http_start": self.http_start_time.strftime('%m-%d %H:%M:%S_%f'),
+            "http_end": _request_end_time.strftime('%m-%d %H:%M:%S_%f'),
+            "http_use": '%s.%s s' % (_request_gap.seconds, _request_gap.microseconds // 1000)
+        }
+        return _performance
 
 
 # @staticmethod
@@ -23,7 +59,7 @@ def api_request_parse(parser=None):
 
 
 # @staticmethod
-def api_response_format(result_list=None, page=None):
+def api_response_format(result_list=None, page=None, performance=None):
     try:
 
         _results = {
@@ -33,6 +69,7 @@ def api_response_format(result_list=None, page=None):
         _response = {
             "code": 1,
             "desc": "success",
+            "perf": performance,
             "resource": SQLConfig.SQLALCHEMY_DATABASE_URI.split(":")[0],
             "result": _results
         }
